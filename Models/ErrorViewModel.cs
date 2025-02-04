1b83c1165cc826c5d57e641a8aca69b8c729d05d
Mustafa Akdaş <makdas163@gmail.com>
2025-02-04 17:14:34 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VakifbankIntern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
